f995ed02f9631eeab11ec39abd2dd6cf7e47d4eb
nazifaraisa44 <160374706+nazifaraisa44@users.noreply.github.com>
2024-09-14 20:11:29 +0600

    Schema of user and complaint

---
+++ server/models/complaint.js
@@ -0,0 +1,14 @@
+
+// models/complaint.js
+
+import mongoose from 'mongoose';
+
+const complaintSchema = new mongoose.Schema({
+  studentID: { type: String, required: true },
+  roomNumber: { type: String, required: true },
+  complaint: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  status: { type: String, default: 'submitted' } // Add status field with default value
+});
+
+export const Complaint = mongoose.model('Complaint', complaintSchema);
